61e6d254cee79197c4af6124d82af43e0c809b63
Sumon <m.sumon@outlook.com>
2023-07-28 00:02:38 +0600

    project initialize and database connected

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("pc builder app server is running");
+});
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const app = require("./app.js");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+async function main() {
+  await mongoose.connect(process.env.DATABASE_URL);
+  //   await mongoose.connect("mongodb://127.0.0.1:27017/pc-builder");
+  console.log("connected to database");
+  app.listen(port, () => {
+    console.log(`pc-builder server listening on ${port}`);
+  });
+}
+main();
